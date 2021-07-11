166aa8327e5ac61b33c8b44713ec725783ed7b9b
qt_coder <65493032+qt-coder@users.noreply.github.com>
2021-07-11 12:40:48 +0800

    Added animations to project pages

--- pages/cath.js
+++ pages/cath.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import styles from '../styles/ProjectPage.module.css'
+import {motion} from 'framer-motion'
+
+const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+            delayChildren: 0.4,
+            staggerChildren: 0.3
+        }
+    }
+}
+
+const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+        y: 0,
+        opacity: 1
+    }
+}
 
 function Cath() {
     return (
-        <div>
-            <h2 className={styles.subtitle}>A discord bot with a dashboard</h2>
-            <a className={styles.title} href='https://github.com/night0721/cath.gq' target='_empty'>QT Drive</a>
-            <p className={styles.desc}>
+        <motion.div variants={container} initial='hidden' animate='visible'>
+            <motion.h2 className={styles.subtitle} variants={item}>A discord bot with a dashboard</motion.h2>
+            <motion.a className={styles.title} href='https://github.com/night0721/cath.gq' target='_empty' variants={item}>QT Drive</motion.a>
+            <motion.p className={styles.desc} variants={item}>
                 Cath.exe is a Discord bot that has a dashboard made in <a>Next JS</a>, <a>Node</a>, and <a>Chakra UI</a>.
                 <br />
                 It was made in conjunction with a team of other developers, and I led the frontend dashboard creation.
@@ -16,9 +37,9 @@ function Cath() {
                 <div className={styles.live}>
                     Check it out <a href='https://cath.gq' target='_empty'>live</a>
                 </div>
-            </p>
-            <img src={'/cath.png'} className={styles.preview} />
-        </div>
+            </motion.p>
+            <motion.img src={'/cath.png'} className={styles.preview} variants={item}/>
+        </motion.div>
     )
 }
 
--- pages/qtdrive.js
+++ pages/qtdrive.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import styles from '../styles/ProjectPage.module.css'
+import {motion} from 'framer-motion'
+
+const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+            delayChildren: 0.4,
+            staggerChildren: 0.3
+        }
+    }
+}
+
+const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+        y: 0,
+        opacity: 1
+    }
+}
 
 function Qtdrive() {
     return (
-        <div>
-            <h2 className={styles.subtitle}>A file storage app</h2>
-            <a className={styles.title} href='https://github.com/qt-coder/qt-drive' target='_empty'>QT Drive</a>
-            <p className={styles.desc}>
+        <motion.div variants={container} initial='hidden' animate='visible'>
+            <motion.h2 className={styles.subtitle} variants={item}>A file storage app</motion.h2>
+            <motion.a className={styles.title} href='https://github.com/qt-coder/qt-drive' target='_empty' variants={item}>QT Drive</motion.a>
+            <motion.p className={styles.desc} variants={item}>
                 QT Drive is a file storage system made using <a>Bootstrap</a>, <a>Firebase</a>, and <a>React JS</a>.
                 <br/>
                 It includes authentication, the ability to upload files, creating folders and more.
@@ -14,9 +35,9 @@ function Qtdrive() {
                 <div className={styles.live}>
                     Check it out <a href='https://qtdrive.netlify.app' target='_empty'>live</a>
                 </div>
-            </p>
-            <img src={'/qtdrivepreview.png'} className={styles.preview}/>
-        </div>
+            </motion.p>
+            <motion.img src={'/qtdrivepreview.png'} className={styles.preview} variants={item}/>
+        </motion.div>
     )
 }
 
